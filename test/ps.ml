2835c08badaec198d28ba3cf934cb2c8aefb9ea2
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2018-06-24 19:42:59 +0200

    Improve the "ps" test

--- test/ps.ml
+++ test/ps.ml
@@ -1,12 +1,16 @@
 open Printf
+module C = Docker.Container
 
 let () =
-  let c = Docker.Container.create "alpine:latest" ["sleep"; "2"]
-            ~name:"some_email_somewhere" in
+  let cmd = ["sleep"; "0.3"] in
+  let c = C.create "alpine:latest" ~name:"waiting_container" cmd in
   printf "Created container id: %s\n%!" c;
-  Docker.Container.start c;
-  let l = Docker.Container.list() in
-  Docker.Container.stop c;
-  Docker.Container.rm c;
+  (try C.start c
+   with Docker.Failure(_, m) -> printf "Docker.Container.start: %s\n" m);
+  let l = C.list() in
+  (try C.stop c
+   with Docker.Failure(_, m) -> printf "Docker.Container.stop: %s\n" m);
+  C.rm c;
   printf "List containers:\n";
-  List.iter (fun c -> printf "id: %s\n" c.Docker.Container.id) l
+  List.iter (fun c -> printf "- id: %s\n  names: %s\n"
+                        c.C.id (String.concat ", " c.C.names)) l
