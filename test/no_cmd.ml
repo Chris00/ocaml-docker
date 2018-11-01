2f752656967518b6422cda0db2643c907f30315a
Christophe Troestler <Christophe.Troestler@umons.ac.be>
2018-11-01 12:53:50 +0100

    Add a test for non-existing Container.Exec.create command

--- test/no_cmd.ml
+++ test/no_cmd.ml
@@ -9,4 +9,21 @@ let () =
     C.start c;
     assert false
   with Docker.Failure _ as e ->
-    printf "Raised %s\n" (Printexc.to_string e)
+    printf "Raised %s\n%!" (Printexc.to_string e)
+
+let () =
+  try
+    let c = C.create "alpine:latest" ["/bin/ash"] ~open_stdin:true in
+    C.start c;
+    let e = C.Exec.create c ["bash"] in
+    let stream = C.Exec.start e in
+    let ty, s = Docker.Stream.read stream in
+    printf "Read %S on %s\n%!" s (match ty with Stdout -> "Stdout"
+                                              | Stderr -> "Stderr");
+    C.stop c;
+    C.rm c;
+    (* assert false *)
+    printf "Unfortunately, Docker does not indicate when a command is not \
+            available.\n"
+  with Docker.Failure _ as e ->
+    printf "Raised %s\n%!" (Printexc.to_string e)
